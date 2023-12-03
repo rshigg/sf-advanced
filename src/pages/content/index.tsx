389a41176b82f91ff63545a9225e14b87cf693c4
rshig <90143161+rshigg@users.noreply.github.com>
2023-12-03 19:51:29 -0330

    clean up styles

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -8,10 +8,6 @@ if (!main) throw new Error("Can't find main element");
 
 const div = document.createElement("div");
 div.id = "__root";
-div.setAttribute(
-  "style",
-  `position: sticky;top: 0;background: inherit;z-index: 100;overflow: hidden;`
-);
 main.prepend(div);
 
 const rootContainer = document.querySelector("#__root");
